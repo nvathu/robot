de030ba04233466cbd307b4a5bdd3ee33dd3c16a
nvathu <fan.mlee2k@gmail.com>
2026-04-01 12:48:02 +0200

    rewrite ResNet18

--- model.py
+++ model.py
@@ -1,25 +1,84 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import models
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=3,  
+            stride=stride,  
+            padding=1,       
+            bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+
+        self.conv2 = nn.Conv2d(
+            out_channels,
+            out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.downsample = None
+
+
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+
+        identity = x  
+
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+  
+        out = out + identity
+        out = self.relu(out)
+
+        return out
+
 
 class ResNetDepth(nn.Module):
-
     def __init__(self):
         super().__init__()
 
-        resnet = models.resnet18(pretrained=True)
+ 
+        self.in_channels = 64
 
-        self.encoder = nn.Sequential(
-            resnet.conv1,
-            resnet.bn1,
-            resnet.relu,
-            resnet.maxpool,
-            resnet.layer1,
-            resnet.layer2,
-            resnet.layer3,
-            resnet.layer4
-        )
+  
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(64, 2, stride=1)
+        self.layer2 = self._make_layer(128, 2, stride=2)
+        self.layer3 = self._make_layer(256, 2, stride=2)
+        self.layer4 = self._make_layer(512, 2, stride=2)
+
+
 
         self.head = nn.Sequential(
             nn.Conv2d(512, 256, 3, padding=1),
@@ -37,11 +96,48 @@ class ResNetDepth(nn.Module):
             nn.Conv2d(64, 32, 3, padding=1),
             nn.ReLU(),
 
-            nn.Conv2d(32, 1, 1)   
+            nn.Conv2d(32, 1, 1)  
         )
 
+
+    def _make_layer(self, out_channels, blocks, stride):
+
+        layers = []
+
+        layers.append(
+            BasicBlock(self.in_channels, out_channels, stride)
+        )
+
+        self.in_channels = out_channels
+
+  
+        for _ in range(1, blocks):
+            layers.append(
+                BasicBlock(self.in_channels, out_channels)
+            )
+
+        return nn.Sequential(*layers)
+
+
     def forward(self, x):
-        x = self.encoder(x)
+
+
+        x = self.conv1(x)   
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.maxpool(x)  
+
+        x = self.layer1(x)    
+        x = self.layer2(x)   
+        x = self.layer3(x)   
+        x = self.layer4(x)    
+
+
         x = self.head(x)
-        x = F.interpolate(x, size=(180,180))
+
+
+        x = F.interpolate(x, size=(180, 180))
+
         return x
+
