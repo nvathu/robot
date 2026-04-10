0efbf72ebf18a4f4ab9e42ed6bab2e430178ba33
nvathu <fan.mlee2k@gmail.com>
2026-04-10 14:13:55 +0200

    measure time and progess bar

--- train.py
+++ train.py
@@ -5,6 +5,8 @@ from torch.utils.tensorboard import SummaryWriter
 
 from dataset import DepthDataset
 from model import ResNetDepth
+from tqdm import tqdm
+import time
 
 import matplotlib.pyplot as plt
 import os
@@ -80,13 +82,14 @@ val_losses = []
 
 fixed_img = None
 fixed_depth = None
+start_time = time.time()
 
 for epoch in range(num_epochs):
 
     model.train()
     train_loss = 0
 
-    for img, depth in train_loader:
+    for img, depth in tqdm(train_loader, desc=f"Epoch {epoch} [Train]"):
 
         img = img.to(device)
         depth = depth.to(device)
@@ -111,7 +114,7 @@ for epoch in range(num_epochs):
     val_loss = 0
 
     with torch.no_grad():
-        for img, depth in val_loader:
+        for img, depth in tqdm(val_loader, desc=f"Epoch {epoch} [Val]"):
             img = img.to(device)
             depth = depth.to(device)
 
@@ -146,6 +149,8 @@ for epoch in range(num_epochs):
         epoch
     )
     print(f"Epoch {epoch}: Train Loss={train_loss:.4f}, Val Loss={val_loss:.4f}")
+    end_time = time.time()
+    print(f"Total training time: {end_time - start_time:.2f} seconds")
 
 
 
